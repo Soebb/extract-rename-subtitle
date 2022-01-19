b8f4c976e6de0e38e331e6435936cb5c094d217b
escape0707 <tothesong@gmail.com>
2022-01-19 13:56:47 +0800

    Revise subtitle_utils.py

--- subtitle_utils.py
+++ subtitle_utils.py
@@ -7,7 +7,22 @@ import re
 simple_ep_pattern = re.compile(r".*\s(\d{2})\s.*")
 
 
-def format_video_by_ep_collection_with_pattern(
+def print_video_by_ep_collection(
+    video_by_ep_collection: dict[str, pathlib.Path]
+) -> None:
+    print("Video Ep;\tVideo path:")
+    print(
+        *itertools.starmap("{};\t{}".format, video_by_ep_collection.items()), sep="\n"
+    )
+
+
+def get_video_collection_with_glob(
+    video_glob: str, video_dir: pathlib.Path = pathlib.Path()
+) -> tuple[pathlib.Path, ...]:
+    return tuple(video_dir.glob(video_glob))
+
+
+def generate_video_by_ep_collection_with_pattern(
     video_collection: tuple[pathlib.Path, ...],
     video_ep_pattern: re.Pattern[str] = simple_ep_pattern,
 ) -> dict[str, pathlib.Path]:
@@ -19,30 +34,17 @@ def format_video_by_ep_collection_with_pattern(
     return video_by_ep_collection
 
 
-def print_video_by_ep_collection(
-    video_by_ep_collection: dict[str, pathlib.Path]
-) -> None:
-    print("Video Ep; Video path:")
-    print(*itertools.starmap("{}; {}".format, video_by_ep_collection.items()), sep="\n")
-
-
-def get_video_collection_with_glob(
-    video_glob: str, video_dir: pathlib.Path = pathlib.Path()
-) -> tuple[pathlib.Path, ...]:
-    return tuple(video_dir.glob(video_glob))
-
-
 def get_video_by_ep_collection_with_glob_and_pattern(
     video_glob: str = "*.mkv",
     video_ep_pattern: re.Pattern[str] = simple_ep_pattern,
     video_dir: pathlib.Path = pathlib.Path(),
 ) -> dict[str, pathlib.Path]:
     video_collection = get_video_collection_with_glob(video_glob, video_dir)
-    return format_video_by_ep_collection_with_pattern(
+    return generate_video_by_ep_collection_with_pattern(
         video_collection, video_ep_pattern
     )
 
 
 def prompt_for_user_confirmation(request_text: str) -> bool:
     user_input = input(request_text + " [Y/n] ")
-    return user_input in ("", "Y", "y")
+    return user_input.lower() in ("", "y")
